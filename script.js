92dce273e86a05610345381be6074177d8028bd8
Leviakc <gomen98.jg@gmail.com>
2023-06-19 09:48:29 -0600

    a tweak in the transition

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 // Wait for the DOM to be fully loaded
 document.addEventListener("DOMContentLoaded", function () {
-// Get the main element
-const main = document.querySelector(".main");
+  // Get the main element
+  const main = document.querySelector(".main");
 
-// Add the "fade-in" class to apply the animation
-main.classList.add("fade-in");
+  // Add the "fade-in" class to apply the animation
+  setTimeout(() => main.classList.add("fade-in"), 1000);
 });
-
